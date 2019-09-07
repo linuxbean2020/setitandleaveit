07c3d11c8bd0a36bd1a327197a5485dabe9481ab
Harshit Nayak <harshitnayak45@gmail.com>
2019-09-07 17:34:04 +0530

    new update

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "client/build")))
 
 // const storage = multer.diskStorage({
 //     destination: function(req,file,cb){
